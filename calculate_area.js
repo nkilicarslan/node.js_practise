656152fb2a8946a22c792f3243d8c9a2a594dcef
nkilicarslan <necatkilicarslann@gmail.com>
2022-07-14 19:07:50 +0300

    area calculation

--- calculate_area.js
+++ calculate_area.js
@@ -1,3 +1,11 @@
-console.log('Please type the radius');
+//console.log('Please type the radius');
 
 const arguments = process.argv.slice(2);
+var pi = 3.14159;
+var area;
+function calculate_area(radius){
+    area = pi * radius * radius;
+}
+
+calculate_area(arguments[0]*1);
+console.log(area);
